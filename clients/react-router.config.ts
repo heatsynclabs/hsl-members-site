0d7e8ef6164dae15eb7fceb8dec5a2af58efe7a5
Bryan Maamo <ninbryan@users.noreply.github.com>
2025-10-20 21:21:39 -0700

    11-add-vscode-settings-and-recommended-settings-and-activate-spa-mode

--- clients/react-router.config.ts
+++ clients/react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from '@react-router/dev/config';
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false, // firefox users do not like ssr... sad
 } satisfies Config;
