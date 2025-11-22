7cde9fa573f159ee78c17c4b329a67f58fe39a04
Etienne Laurin <etienne@atnnn.com>
2025-11-22 16:35:30 -0700

    ssr to spa

--- clients/app/routes/api.tsx
+++ clients/app/routes/api.tsx
@@ -1,10 +1,10 @@
 import type { Route } from './+types/api';
 
-export function loader() {
+export function clientLoader() {
   return { text: 'butt' };
 }
 
-export async function action({ request }: Route.ActionArgs) {
+export async function clientAction({ request }: Route.ActionArgs) {
   let formData = await request.formData();
   let text = formData.get('text');
   return { text, yes: true, num: 1 };
