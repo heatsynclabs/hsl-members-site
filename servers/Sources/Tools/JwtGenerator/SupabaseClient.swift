4d5659bb8b727e5e47053afc454640d119f99ed2
Austin Townsend <130945920+Pancakeshack@users.noreply.github.com>
2025-11-27 00:00:07 -0700

    update action

--- servers/Sources/Tools/JwtGenerator/SupabaseClient.swift
+++ servers/Sources/Tools/JwtGenerator/SupabaseClient.swift
@@ -1,3 +1,4 @@
+// swiftlint :disable all
 import struct Foundation.URL
 import class Supabase.SupabaseClient
 
@@ -11,9 +12,7 @@ struct SupabaseConfig {
             supabaseURL: URL(string: "https://fcctfigwgmjcpkgoorxr.supabase.co")!,
             // Public key, so this is safe to commit
             supabaseKey:
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjY3RmaWd3Z21qY3BrZ29"
-                + "vcnhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NzI0OTMsImV4cCI6MjA3ODE0ODQ5M30._tLpbbANT"
-                + "eFRrZzl2qCVrDDeSYW4kC5a1_u57dqTCbE"
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjY3RmaWd3Z21qY3BrZ29vcnhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NzI0OTMsImV4cCI6MjA3ODE0ODQ5M30._tLpbbANTeFRrZzl2qCVrDDeSYW4kC5a1_u57dqTCbE"
         )
     }
 }
