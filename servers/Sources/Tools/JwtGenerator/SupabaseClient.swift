ec5bbf397828f37f58cd3e7a24cd24aa766d010f
Austin Townsend <130945920+Pancakeshack@users.noreply.github.com>
2025-11-27 01:05:02 -0700

    fix supabase issue

--- servers/Sources/Tools/JwtGenerator/SupabaseClient.swift
+++ servers/Sources/Tools/JwtGenerator/SupabaseClient.swift
@@ -13,7 +13,14 @@ struct SupabaseConfig {
             // Public key, so this is safe to commit
             supabaseKey:
                 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjY3RmaWd3Z21qY3BrZ29vcnhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NzI0OTMsImV4cCI6MjA3ODE0ODQ5M30._tLpbbANTeFRrZzl2qCVrDDeSYW4kC5a1_u57dqTCbE",
-            options: .init()
+            options: .init(
+                db: .init(),
+                auth: .init(),
+                global: .init(),
+                functions: .init(),
+                realtime: .init(),
+                storage: .init()
+            )
         )
     }
 }
